d774e5327a0cccf665da3d5bebe5a86808c5ac70
Chain <ntrungtn@gmail.com>
2022-10-06 01:57:27 +0700

    feat: setup (css-loader, enums), setting swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   css: ['@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/dayjs'],
+  plugins: ['@/plugins/dayjs', '@/plugins/swiper'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
